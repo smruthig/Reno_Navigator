c13193902a6e61dc46f75a2b847bc73c62dbf7d7
Smruthi Gowtham <smruthig01@gmail.com>
2021-10-30 17:19:44 +0530

    added simple queries

---
+++ assignment 3/simple_queries.sql
@@ -0,0 +1,41 @@
+-- 1. DESIGNER:
+	SELECT * FROM designedBy AS des, project AS proj, siteDetails AS site,  
+	design AS d, room AS r, hasRoom AS hr, designIncludesProducts AS dip,
+	product AS prod
+	WHERE des.projectID = proj.projectID
+	AND proj.siteID = site.siteDetails
+	AND hr.projectID = proj.projectID
+	AND hr.roomID = r.roomID 
+	AND r.designID = d.designID
+	AND dip.designID = d.designID
+	AND prod.productID = dip.productID;
+
+	//Or do we need to split this into 3 different queries?
+	
+-- 2. PROJECT MANAGER
+--a.
+	SELECT * FROM managedBy AS m, project AS proj, siteDetails AS site 
+	WHERE m.projectID = proj.projectID
+	AND proj.siteID = site.siteID;
+-- b.  
+	SELECT projectID, customerID, customerName, customerPhNo, customerEmailID, customerAddress from
+	project as proj, customer as cust, managedBy as m 
+	WHERE m.projectID = proj.projectID
+	AND proj.customerID = cust.customerID;
+-- c.	
+	SELECT * FROM managedBy AS m, project AS proj,  
+	design AS d, room AS r, hasRoom AS hr, designIncludesProducts AS dip,
+	product AS prod
+	WHERE m.projectID = proj.projectID
+	AND hr.projectID = proj.projectID
+	AND hr.roomID = r.roomID 
+	AND r.designID = d.designID
+	AND dip.designID = d.designID
+	AND prod.productID = dip.productID;
+ 
+-- 3. ADMIN
+-- a. 
+	SELECT * FROM projects AS p, siteDetails AS s WHERE p.siteID = s.siteID;
+-- b. 
+	SELECT * FROM customer;
+
