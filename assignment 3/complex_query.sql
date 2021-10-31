951006e41b79683cf5972050985d590f1075a585
ubuntu <compspandan@gmail.com>
2021-10-31 15:23:59 +0530

    Fixed insert.sql for room and hasRoom

--- assignment 3/complex_query.sql
+++ assignment 3/complex_query.sql
@@ -8,3 +8,7 @@ select * from customer where customerID IN (select customerID from project where
 
 -- List of rooms in a project with projectID=2 with respective designs and products used in each design
 select * from hasRoom natural join room where projectID=2;
+
+-- List of contractors for a project with projectID=4
+-- List of designers working on a project
+-- List of designer who are working on less than 2 projects
