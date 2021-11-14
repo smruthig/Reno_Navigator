6275189e737d004657111ac6884630664c2d7c94
ubuntu <compspandan@gmail.com>
2021-11-14 11:56:22 +0530

    Added user.sql

---
+++ assignment 3/user.sql
@@ -0,0 +1,5 @@
+create user projectManager with password '1234' createdb;
+create user designer with password '1234' createdb;
+create user admin with password '1234' createdb;
+create user companyManager with password '1234' createdb;
+create user accountant with password '1234' createdb;
