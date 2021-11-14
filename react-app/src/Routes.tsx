39a2031f21a8ca7502ad71e9251c2b10c687beec
ubuntu <compspandan@gmail.com>
2021-11-14 13:27:17 +0530

    added git ignore to flask app

--- react-app/src/Routes.tsx
+++ react-app/src/Routes.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
 import {Login} from "./components/Login";
+import { SignUp } from "./components/SignUp";
 export default function Routes()
 {
 	return (
 		<Router>
 			<Switch>
 				<Route path='/' element={<Login/>}/>
-				<Route path='/success' element={<h1>Success!!</h1>}/>
+				<Route path='/signup' element={<SignUp/>}/>
 			</Switch>
 		</Router>
 	);
---
+++ react-app/src/components/SignUp.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Box, Heading } from "@chakra-ui/layout";
+import axios from "../utils/axios";
+import { useForm } from "react-hook-form";
+import { AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
+
+interface SignUpFormProps {
+  emailId: string;
+  password: string;
+}
+export const SignUp: React.FC = () => {
+  const { handleSubmit, register } = useForm<SignUpFormProps>();
+  const navigate = useNavigate();
+  async function onSubmit(formdata: any) {
+    const {data}: AxiosResponse = await axios.post("/login", formdata);
+    //successful login
+    if (data.message === "success") {
+      navigate(`/user/${data.employeeId}`);
+    }
+    //new user must sign up
+    else if(data.message === "no user"){
+      navigate('/signup'); 
+    }
+  }
+
+  return (
+    <Box mx="auto" my="auto" w="50rem" h="50rem" p="5rem">
+      <Heading textAlign="center">Sign Up</Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl id="email-id" isRequired>
+          <FormLabel>Email ID</FormLabel>
+          <Input {...register("emailId")} placeholder="Email Id" />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input {...register("password")} placeholder="Password" />
+        </FormControl>
+        <Button my="5" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+};
