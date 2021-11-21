9c9fa9e15e3ed46dd50f5afc7058d7b5cce5b5c8
Smruthi Gowtham <smruthig01@gmail.com>
2021-11-21 16:36:20 +0530

    created front end for des

--- react-app/src/Routes.tsx
+++ react-app/src/Routes.tsx
@@ -3,6 +3,7 @@ import {Login} from "./components/Login";
 import AddProjectForm from "./components/projectManager/AddProjectForm";
 import ProjectDetails from "./components/projectManager/ProjectDetails";
 import ProjectList from "./components/projectManager/ProjectList";
+import ProjectListDes from "./components/designer/ProjectList";
 import { SignUp } from "./components/SignUp";
 export default function Routes()
 {
@@ -12,6 +13,7 @@ export default function Routes()
 				<Route path='/' element={<Login/>}/>
 				<Route path='/signup' element={<SignUp/>}/>
 				<Route path='/projectManager' element={<ProjectList/>}/>
+				<Route path='/designer' element={<ProjectListDes/>}/>
 				<Route path='/projectManager/:projectId' element={<ProjectDetails/>}/>
 				<Route path='/addproject' element={<AddProjectForm/>}/>
 			</Switch>
