0e1adae03b41ef4c7dc9108693a69702a14d2f82
Smruthi Gowtham <smruthig01@gmail.com>
2021-11-21 17:24:39 +0530

    backend for des proj

--- flask-app/routes/designer.py
+++ flask-app/routes/designer.py
@@ -25,6 +25,28 @@ def get_projects(employee_id):
 	finally:
 		cursor.close()
 
+# get project details given project_id
+@designer_blueprint.route('/designer/<string:employee_id>/<string:project_id>')
+def get_project_by_id(employee_id,project_id):
+	try:
+		cursor = g.db.cursor(cursor_factory=RealDictCursor)
+
+		cursor.execute(f"SELECT * FROM project where projectid={project_id}")
+		project = cursor.fetchone()
+
+		cursor.execute(f"SELECT houseNo, street, pincode, city, state, length, breadth from sitedetails where siteid={project['siteid']}")
+		site = cursor.fetchone()
+
+		cursor.execute(f"SELECT roomid, roomname, roomsize, designid, productid, typename, productcost, description from (SELECT * from (SELECT * from hasRoom natural join room WHERE projectID={project_id}) as x natural join designIncludesProducts) as y natural join product;")
+		des_for_rooms = cursor.fetchall()
+		
+		return jsonify(project=project,site=site, des_for_rooms=des_for_rooms)
+	except(Exception, psycopg2.Error) as error:
+		print(error)
+		return Response(error,status=500)
+	finally:
+		cursor.close()
+
 # function called before request is closed
 @designer_blueprint.teardown_app_request
 def teardown_db(exception):
